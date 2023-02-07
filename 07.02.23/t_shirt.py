07751662fe22e0614dfe67e4979a0db9e99194a6
jehanzeb-ayaz <124182379+jehanzeb-ayaz@users.noreply.github.com>
2023-02-07 20:52:20 +0100

    Some example scripts and exercises

---
+++ 07.02.23/t_shirt.py
@@ -0,0 +1,8 @@
+def make_shirt(size, message):
+  print(f"The shirt is size {size} with message '{message}' printed on it.")
+
+# using positional arguments
+make_shirt("Large", "I love Python")
+
+# using keyword arguments
+make_shirt(message="Life is good", size="Medium")
