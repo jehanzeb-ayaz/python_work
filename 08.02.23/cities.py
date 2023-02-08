161c3a97815dd9a6cd92f0a3aa2e421b0af15cbf
jehanzeb-ayaz <124182379+jehanzeb-ayaz@users.noreply.github.com>
2023-02-08 08:23:50 +0100

    Some example scripts and exercises

---
+++ 08.02.23/cities.py
@@ -0,0 +1,6 @@
+def describe_city(city, country="unknown"):
+    print(f"{city} is in {country}.")
+
+describe_city("Reykjavik", "Iceland")
+describe_city("London", "England")
+describe_city("Sydney")
---
+++ 08.02.23/large_shirts.py
@@ -0,0 +1,6 @@
+def make_shirt(size="large", message="I love Python"):
+    print(f"Making a {size} shirt with the message '{message}'")
+
+make_shirt()
+make_shirt(size="medium")
+make_shirt(message="Coding is fun!", size="small")
